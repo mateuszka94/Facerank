b4930b9dc36b3e7dd1e2b825a70afff86548c061
Mateusz Kaleciński <mateuszka94@gmail.com>
2015-05-15 10:54:41 +0200

    Zmiany w PhotoManager.

--- app/src/main/java/com/example/mateusz/facerank/Main.java
+++ app/src/main/java/com/example/mateusz/facerank/Main.java
@@ -1,36 +1,37 @@
 package com.example.mateusz.facerank;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 //TODO: access token; GET... /me;
 
 public class Main extends Activity {
 	Random random = new Random();
-	int[] usersId;
+	ArrayList<String> usersIds;
+
+    ImageView right;
+    ImageView left;
+
+    PhotoManager photoManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        left = ( ImageView ) findViewById( R.id.leftImage );
+        right = ( ImageView ) findViewById( R.id.rightImage );
+
+
+        usersIds = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.profile_id)));
+
+        photoManager = new PhotoManager();
+
     }
 
     @Override
@@ -43,28 +44,8 @@ public class Main extends Activity {
         super.onPause();
     }
 
-	private int[] choosePictures() {
-		int[] pictureIds = new int[ 2 ];
-		int i = random.nextInt( usersId.length );
-		pictureIds[ 0 ] = usersId[ i ];
-		i = random.nextInt( usersId.length );
-		while( i == pictureIds[ 0 ] )
-			i = random.nextInt( usersId.length );
-		return pictureIds;
-	}
-
     public void OnClick( View view ){
         //losuj kolejne zdjęcia i je wyświetl
-		int[] pictureIds = choosePictures();
-		ImageView left = ( ImageView ) findViewById( R.id.leftImage );
-		ImageView right = ( ImageView ) findViewById( R.id.rightImage );
-			for( int i = 0; i < 2; i++ ) {
-				ImageView imageView;
-				if( i == 0 )
-					imageView = left;
-				else
-					imageView = right;
-				Picasso.with( this ).load("http://graph.facebook.com/" + pictureIds[ i ] + "/picture?type=large").into(imageView);
-			}
+        photoManager.choosePictures(left, right, usersIds, getApplicationContext());
 	}
 }
--- app/src/main/java/com/example/mateusz/facerank/PhotoManager.java
+++ app/src/main/java/com/example/mateusz/facerank/PhotoManager.java
@@ -1,8 +1,28 @@
 package com.example.mateusz.facerank;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Mateusz on 2015-04-26.
  */
 public class PhotoManager {
     //prawdopodobnie będzie niepotrzebny
+    Random random = new Random();
+
+    public void choosePictures(ImageView leftView, ImageView rightView, ArrayList<String> facebookIDs, Context context) {
+
+        int leftId = random.nextInt(facebookIDs.size());
+        int rightId = random.nextInt(facebookIDs.size());
+
+        Picasso.with(context).load("http://graph.facebook.com/" + leftId + "/picture?type=large").into(leftView);
+        Picasso.with(context).load("http://graph.facebook.com/" + rightId + "/picture?type=large").into(rightView);
+    }
+
+
 }
